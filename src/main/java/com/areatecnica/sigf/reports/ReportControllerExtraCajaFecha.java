fd965e5a20534bf8ff9501db06cd7fc7dce4d734
ianfranco <i.conchaherrera@uandresbello.edu>
2019-03-02 08:14:09 -0300

    -	Correxiones informes

--- src/main/java/com/areatecnica/sigf/reports/ReportControllerExtraCajaFecha.java
+++ src/main/java/com/areatecnica/sigf/reports/ReportControllerExtraCajaFecha.java
@@ -5,20 +5,14 @@
  */
 package com.areatecnica.sigf.reports;
 
-import com.areatecnica.sigf.controller.reports.InformeConsumoCombustibleController;
 import com.areatecnica.sigf.controller.util.JsfUtil;
 import com.areatecnica.sigf.dao.impl.ICajaRecaudacionDaoImpl;
 import com.areatecnica.sigf.entities.CajaRecaudacion;
 import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
@@ -27,45 +21,34 @@ import javax.faces.view.ViewScoped;
  *
  * @author ianfr
  */
-@Named(value = "reportControllerGeneralMensual")
+@Named(value = "reportControllerExtraCajaFecha")
 @ViewScoped
-public class ReportControllerGeneralMensual implements Serializable {
+public class ReportControllerExtraCajaFecha implements Serializable {
 
     private List<CajaRecaudacion> items;
     private CajaRecaudacion selected;
     private Date fecha;
     private String informe;
-    private int mes;
-    private int anio;
-    private final static SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/01");
 
     /**
      * Creates a new instance of ReportControllerGuiaCajaFecha
      */
-    public ReportControllerGeneralMensual() {
+    public ReportControllerExtraCajaFecha() {
 
     }
- 
+
     @PostConstruct
     private void init() {
         this.items = new ICajaRecaudacionDaoImpl().findAll();
-        this.items.add(0, new CajaRecaudacion(0, "Todas", true, true));
-
-        this.informe = "inf-recaudacion_general";
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-
-        this.mes = calendar.get(Calendar.MONTH) + 1;
-        this.anio = calendar.get(Calendar.YEAR);
-        setFecha();
-        System.err.println("primera fecha: "+this.fecha);
+        this.fecha = new Date();
+        this.informe = "inf-recaudacion_extra_caja_fecha";
     }
 
     public Map<String, Object> getMap() {
         Map<String, Object> map = new HashMap();
 
-        map.put("fecha", this.fecha);
-        //map.put("caja", selected.getCajaRecaudacionId());
+        map.put("fecha", fecha);
+        map.put("caja", selected.getCajaRecaudacionId());
 
         return map;
     }
@@ -98,12 +81,6 @@ public class ReportControllerGeneralMensual implements Serializable {
         this.informe = informe;
     }
 
-    public void setPath() {
-        if (this.selected.getCajaRecaudacionId() == null) {
-            this.informe = "inf-recaudacion_general_caja";
-        }
-    }
-
     public String getInforme() {
         return informe;
     }
@@ -111,31 +88,4 @@ public class ReportControllerGeneralMensual implements Serializable {
     public void falta() {
         JsfUtil.addErrorMessage("No se encuentran registros");
     }
-
-    public int getAnio() {
-        return anio;
-    }
-
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
-
-    public int getMes() {
-        return mes;
-    }
-
-    public void setMes(int mes) {
-        this.mes = mes;
-    }
-
-    public void setFecha() {
-        try {
-            
-            this.fecha = this.sdf.parse(this.anio+"/"+this.mes+"/01");
-            System.err.println("NUEVA FECHA: "+this.fecha);
-        } catch (ParseException ex) {
-            Logger.getLogger(InformeConsumoCombustibleController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
 }
